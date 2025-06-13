43b2efd8a0a0c17e870f78d70583031a2f4a9e0f
Sweta Perutty <swetaperutty@Swetas-MacBook-Air.local>
2025-06-13 10:26:32 -0500

    third commit

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,11 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("This is project version 2");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
+        System.out.println("This is project version 3");
     }
 }
-//done with version two
+//done with version three
