20647ab4a8286dc32317562deb3d81da32751a6d
SwetaPerutty <swetaperutty@gmail.com>
2025-06-16 17:24:40 -0500

    person2commit

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String[] args) {
 
-        System.out.println("This is project version 5");
+        System.out.println("This is project version 6 made by person 2");
     }
 }
-//done with version five
+//done with version six
